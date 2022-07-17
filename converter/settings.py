ff69a560dd02acb386935498157eab43b2af45f3
Mikhael Korneev <korneevm@gmail.com>
2022-07-17 11:06:39 +0400

    Working certificates

--- converter/settings.py
+++ converter/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
+AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
--- converter/views.py
+++ converter/views.py
@@ -1,43 +1,41 @@
-from airtable import Airtable
-from django.shortcuts import render
-from converter.config import base_key, api_key
-import pdfkit
-from django.template.loader import get_template
-from datetime import datetime
+
 import locale
+from datetime import date, datetime
+
+from django.shortcuts import render
+from django.conf import settings
+import pdfkit
+from pyairtable import Table
+from pyairtable.formulas import match
 
 
 def init(request):
     # connection with Airtable
-    airtable = Airtable(base_key, 'previous_courses_data', api_key)
+    formula = match({"course_number": "25 набор", "project_presented": 1})
+    table = Table(settings.AIRTABLE_API_KEY, settings.AIRTABLE_BASE_ID, 'current_course')
     # data processing
-    template = get_template('diploma.html')
-    for script in airtable.get_all():
-        is_project = script['fields'].get('is_project', None)
-        current_course = script['fields'].get('course', None)
-        if is_project == True and current_course == '16':
-            name = script['fields']['first_name']
-            surname = script['fields']['last_name']
-            date = script['createdTime']
-            date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
-            locale.setlocale(locale.LC_TIME, "ru_RU")
-            date = datetime.strftime(date, "%d %B %Y")
-            html = render(request, 'diploma.html', {'name': name, 'surname': surname, 'date': date})
-            result = (html.content).decode('utf-8')
-            config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-            options = {
-                'dpi': 300,
-                'page-size': 'A4',
-                'orientation': 'landscape',
-                'margin-top': '0',
-                'margin-right': '0.',
-                'margin-bottom': '0.',
-                'margin-left': '0',
-                'encoding': "UTF-8",
-                'custom-header': [
-                    ('Accept-Encoding', 'gzip')
-                ],
-                'no-outline': None
-            }
-            pdfkit.from_string(result, 'result/' + '{}-{}.pdf'.format(name, surname), configuration=config, options=options)
-    return render(request, 'diploma.html')
+    for script in table.all(formula=formula):
+        name = script['fields']['first_name']
+        surname = script['fields']['last_name']
+        cert_date = date(2022, 4, 16)
+        locale.setlocale(locale.LC_TIME, "ru_RU")
+        cert_date = datetime.strftime(cert_date, "%d %B %Y")
+        html = render(request, 'diploma_ru.html', {'name': name, 'surname': surname, 'date': cert_date})
+        result = (html.content).decode('utf-8')
+        config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+        options = {
+            'dpi': 300,
+            'page-size': 'A4',
+            'orientation': 'landscape',
+            'margin-top': '0',
+            'margin-right': '0.',
+            'margin-bottom': '0.',
+            'margin-left': '0',
+            'encoding': "UTF-8",
+            'custom-header': [
+                ('Accept-Encoding', 'gzip')
+            ],
+            'no-outline': None
+        }
+        pdfkit.from_string(result, 'result/' + '{}-{}.pdf'.format(name, surname), configuration=config, options=options)
+    return render(request, 'diploma_ru.html')
